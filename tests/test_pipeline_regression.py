53c0e87fc73f9e9f08f435d3daaa69f0ac31bb5b
dedi <descobardelrio@gmail.com>
2026-09-14 13:34:46 +0200

    added assertions to test_pipeline to make sure refactoring doesn't change the core algorithm

--- tests/test_pipeline_regression.py
+++ tests/test_pipeline_regression.py
@@ -20,5 +20,15 @@ def test_pipeline():
     pipeline = VisualOdometryPipeline(config)
     pipeline.run()
 
-    assert len(pipeline.global_camera_poses) > 0
-    assert len(pipeline.global_landmarks) > 0
+    assert len(pipeline.global_camera_poses) == 14
+    assert len(pipeline.global_landmarks) == 627
+
+    expected_pose_0 = np.array([1.0, 0.0017, -0.0016])
+    np.testing.assert_allclose(
+        pipeline.global_camera_poses[0][:3, 3], expected_pose_0, atol=1e-3
+    )
+
+    expected_pose_last = np.array([3.5740, -0.0148, -0.0168])
+    np.testing.assert_allclose(
+        pipeline.global_camera_poses[-1][:3, 3], expected_pose_last, atol=1e-3
+    )
